62b15710d69a6c1143b4a07473b95b33756031c9
Nilay <nilaykamat03@gmail.com>
2022-04-17 21:38:02 +0530

    edited music_cmds.py

--- music_cmds.py
+++ music_cmds.py
@@ -2,40 +2,24 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
-
-queue={}
-
 class Music(commands.Cog):
     def __init__(self, client):
         self.client=client
 
     @commands.command(name="play", help="plays music by using Youtube URL.")
-    async def play(self, ctx, url)->None:
-        global queue
-        _key=str(ctx.guild.id)
-        if _key in queue:
-            queue[_key].append(url)
-            await ctx.send("Your song has been queued")
-            return
-        else:
-            queue[_key] = [url]
+    async def play(self, ctx, url):
         if(ctx.author.voice):
-            while len(queue[_key])!=0:
-                #ctx.voice_client.stop()     #stops the song.
-                FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-                YDL_OPTIONS = {'format': "bestaudio"}
-                vc=ctx.voice_client
+            ctx.voice_client.stop()     #stops the song.
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            YDL_OPTIONS = {'format': "bestaudio"}
+            vc=ctx.voice_client
 
-                with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-                    info = ydl.extract_info(queue[_key][0], download=False)
-                    url2=info['formats'][0]['url']
+            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+                url2=info['formats'][0]['url']
 
-                    source=await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-                    vc.play(source)
-
-                queue[_key].pop(0)
-                if not queue[_key]:
-                    del queue[_key]
+                source=await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+                vc.play(source)
         else: 
             await ctx.send("I am terribly sorry, but I cannot join you as you are not in a voice channel.")
 
